e3ccbf199480076ef7ee684893bbc88c136f2dd7
meeran-jeelani <meeranj10@gmail.com>
2026-03-13 20:34:33 +0530

    Add Week 7 Pset

--- Week 7/numb3rs/numb3rs.py
+++ Week 7/numb3rs/numb3rs.py
@@ -3,7 +3,6 @@ import re
 def main():
     print(validate(input("IPv4 Address: ")))
 
-
 def validate(ip):
     pattern = r"^(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})(\.(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})){3}$"
     return re.search(pattern, ip) is not None
