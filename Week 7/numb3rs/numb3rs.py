a7441c5f1a5b9917d044b30b9c032495246cb99f
meeran-jeelani <meeranj10@gmail.com>
2026-03-15 22:55:19 +0530

    Add Week 7 Pset

--- Week 7/numb3rs/numb3rs.py
+++ Week 7/numb3rs/numb3rs.py
@@ -5,6 +5,5 @@ def main():
 def validate(ip):
     pattern = r"^(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})(\.(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})){3}$"
     return re.search(pattern, ip) is not None
-
 if __name__ == "__main__":
     main()
