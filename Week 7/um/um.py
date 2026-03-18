3835c22f63334515c0b7b36f1944b2fb3d5bba5f
meeran-jeelani <meeranj10@gmail.com>
2026-03-18 23:21:58 +0530

    Add Week 7 Pset

--- Week 7/um/um.py
+++ Week 7/um/um.py
@@ -2,7 +2,6 @@ import re
 
 def main():
     print(count(input("Text: ")))
-
 def count(s):
     # Match if "um" is in the beginning (^) or end ($) of a string input
     # Match if the character before and after "um" is not alphanumeric (punctuation / space is ok)
