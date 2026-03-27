33e358a8bb1916661f88730d7c149de8d42e8b32
meeran-jeelani <meeranj10@gmail.com>
2026-03-27 23:38:46 +0530

    Add Week 7 Pset

--- Week 7/watch/watch.py
+++ Week 7/watch/watch.py
@@ -1,7 +1,6 @@
 import re
 def main():
     print(parse(input("HTML: ")))
-
 def parse(s):
     if link := re.search(r"<iframe src=\"https?://(www\.)?youtube\.com/embed/([a-zA-Z0-9]+)\"></iframe>", s):
         return f"https://youtu.be/{link.group(2)}"
